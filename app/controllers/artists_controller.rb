10b6bf4bf881b05e7763c0fba93cb2504e78d3cc
ayakuma309 <ayako.30901@gmail.com>
2023-07-17 15:14:57 +0900

    add: アーティスト情報の保存

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,29 @@
 class ArtistsController < ApplicationController
+
+  def index
+    @artists= Artist.all
+  end
   def search
     if params[:search].present?
       @artists = RSpotify::Artist.search(params[:search])
     end
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    respond_to do |format|
+      if @artist.save
+        format.html { redirect_to artists_path, notice: 'Artist was successfully created.' }
+        format.json { render :show, status: :created, location: @artist }
+      else
+        format.html { render :new }
+        format.json { render json: @artist.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def artist_params
+    params.permit(:artist_name, :artist_image, :spotify_id)
   end
 end
