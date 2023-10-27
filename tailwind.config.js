634e26597312b049601fe8ce1715b20a7606f848
haganenoubik <haganenoubik@gmail.com>
2023-10-28 02:13:36 +0900

    :art: Chore: 諸々調整

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: [
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#00a3af",
+          "secondary": "#008899",
+          "accent": "#e17b34",
+          "neutral": "#18141f",
+          "base-100": "#FFFFFF",
+          "info": "#4d4398",
+          "success": "#99f6e4",
+          "warning": "#fde68a",
+          "error": "#ef857b",
+        },
+      },
+    ],
+  },
+    plugins: [
     require("daisyui"),
   ],
 }
